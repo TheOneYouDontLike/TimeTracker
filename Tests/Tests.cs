963b807df7d7eb244cab39463fa1d9f44623df67
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2014-10-21 22:02:12 +0200

    It is possible to count activities duration and calculate statistics timespan for given activities. It takes the smalles date from all activities and calculates total days since that date.

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,6 +1,8 @@
 ﻿namespace Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using NUnit.Framework;
 
     [TestFixture]
@@ -11,19 +13,86 @@
         {
             new ActivityItem(DateTime.Now, 120, ActivityType.Movie);
         }
+
+        [Test]
+        public void ShouldBePossibleToCountAllActivitiesDuration()
+        {
+            // given
+            var listOfActivities = new List<ActivityItem>
+            {
+                new ActivityItem(DateTime.Now, 100, ActivityType.Movie),
+                new ActivityItem(DateTime.Now, 120, ActivityType.Movie),
+                new ActivityItem(DateTime.Now, 80, ActivityType.Movie)
+            };
+
+            // when
+            var duration = listOfActivities.Sum(activityItem => activityItem.Duration);
+
+            // then
+            Assert.That(duration, Is.EqualTo(300));
+        }
+
+        [Test]
+        public void ShouldBePossibleToDetermineStatisticsTimeSpan()
+        {
+            // given
+            var listOfActivities = new List<ActivityItem>
+            {
+                new ActivityItem(new DateTime(2014,01,01), 100, ActivityType.Movie),
+                new ActivityItem(new DateTime(2014,05,01), 120, ActivityType.Movie),
+                new ActivityItem(new DateTime(2014,10,01), 80, ActivityType.Movie)
+            };
+
+            // when
+            var statiscticsTime = new Statisctics(listOfActivities).TimeSpan();
+
+            // then
+            var timeSpan = DateTime.Now.Subtract(new DateTime(2014, 01, 01));
+            var timeSpanToInt = Convert.ToInt32(Math.Floor(timeSpan.TotalDays));
+            Console.WriteLine(timeSpan.TotalDays);
+            Assert.That(statiscticsTime, Is.EqualTo(timeSpanToInt));
+        }
+    }
+
+    public class Statisctics
+    {
+        private readonly List<ActivityItem> _listOfActivities;
+
+        public Statisctics(List<ActivityItem> listOfActivities)
+        {
+            _listOfActivities = listOfActivities;
+        }
+
+        public int TimeSpan()
+        {
+            var smallestDate = _listOfActivities.Min(activity => activity.Date);
+            var totalDays = DateTime.Now.Subtract(smallestDate).TotalDays;
+            
+            return Convert.ToInt32(Math.Floor(totalDays));
+        }
     }
 
     public class ActivityItem
     {
         private readonly DateTime _date;
-        private readonly int _lenght;
+        private readonly int _duration;
         private readonly ActivityType _activityType;
 
-        public ActivityItem(DateTime date, int lenght, ActivityType activityType)
+        public ActivityItem(DateTime date, int duration, ActivityType activityType)
         {
             _date = date;
-            _lenght = lenght;
+            _duration = duration;
             _activityType = activityType;
+        }
+
+        public int Duration
+        {
+            get { return _duration; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
         }
     }
 
