588021e64506af64ea4219b0204b2c44ea1a5714
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2014-10-21 21:19:25 +0200

    It is possible to create new ActivityItem.

---
+++ Tests/Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ShouldBePossibleToCreateNewItem()
+        {
+            new ActivityItem(DateTime.Now, 120, ActivityType.Movie);
+        }
+    }
+
+    public class ActivityItem
+    {
+        private readonly DateTime _date;
+        private readonly int _lenght;
+        private readonly ActivityType _activityType;
+
+        public ActivityItem(DateTime date, int lenght, ActivityType activityType)
+        {
+            _date = date;
+            _lenght = lenght;
+            _activityType = activityType;
+        }
+    }
+
+    public enum ActivityType
+    {
+        Movie,
+        Series
+    }
+}
