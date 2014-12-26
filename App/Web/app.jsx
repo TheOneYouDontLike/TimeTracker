0cb51c0f4dd9af0bc4a09a585df3dbf5e1a086a3
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2014-12-26 18:46:21 +0100

    Removed posting new activity during every page reload.

--- App/Web/app.jsx
+++ App/Web/app.jsx
@@ -7,23 +7,9 @@ var ActivitiesBox = require('./js/activitiesBox.jsx');
 var mainContainerDiv = document.getElementById('main-container');
 
 request
-	.post('/activities')
-	.set('Content-Type', 'application/json')
-	.send({
-			name: 'Jurassic Park',
-			date: '2014-01-01',
-			duration: 120,
-			activityType: 'Movie',
-			watchedInCinema: false
-		})
-	.end(getDataFromApi);
-
-function getDataFromApi() {
-	request
-	.get('/activities')
-	.accept('application/json')
-	.end(displayActivities);
-}
+.get('/activities')
+.accept('application/json')
+.end(displayActivities);
 
 function displayActivities(response) {
 	var activities = response.body;
