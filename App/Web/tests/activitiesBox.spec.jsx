53e6b25e3461227ef79b302998fa8aa3ce84126e
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2014-12-28 23:56:26 +0100

    Checking if GET request to /activities was called after ActivitiesBox did mount.

--- App/Web/tests/activitiesBox.spec.jsx
+++ App/Web/tests/activitiesBox.spec.jsx
@@ -7,7 +7,6 @@ var sinon = require('sinon');
 global.document = jsdom('<html><head></head><body></body></html>');
 global.window = document.parentWindow;
 global.navigator = window.navigator;
-global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
 
 var React = require('react');
 var TestUtils = require('react/addons').addons.TestUtils;
@@ -17,6 +16,11 @@ var ActivitiesBox = require('../js/activitiesBox.jsx');
 describe('activities-box', function () {
 	it('should render correctly with table and activity form inside', function () {
 		// given
+		var requests = [];
+
+		global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
+		global.XMLHttpRequest.onCreate = function (req) { requests.push(req); };
+
 		// when
 		var rendered = TestUtils.renderIntoDocument(<ActivitiesBox />);
 		
@@ -25,5 +29,9 @@ describe('activities-box', function () {
 		var renderedForm = TestUtils.scryRenderedDOMComponentsWithClass(rendered, 'activity-form');
 		assert.that(renderedActivities.length, is.equalTo(1));
 		assert.that(renderedForm.length, is.equalTo(1));
+		assert.that(requests[0].url, is.equalTo('/activities'));
+		assert.that(requests[0].method.toLowerCase(), is.equalTo('get'));
+
+		global.XMLHttpRequest.restore();
 	});
 });
