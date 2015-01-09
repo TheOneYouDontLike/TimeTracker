6dd2b353a5f7fb2ce859e681ca296ebc28289913
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-01-09 23:41:09 +0100

    Implemented inline change of Activity name.

--- App/Web/js/activitiesTable.jsx
+++ App/Web/js/activitiesTable.jsx
@@ -3,16 +3,26 @@
 var React = require('react');
 
 var ActivitiesTable = React.createClass({
+    onBlurChangeEvent: function (event) {
+        var newStateValue = {};
+        newStateValue[event.target.name] = event.target.value;
+
+        // send update to activityService
+        console.log(newStateValue);
+        console.log(event.target.getAttribute('data-activityid'));
+    },
+
     render: function () {
+        var self = this;
         var activitiesNodes = this.props.data.map(function (activity) {
             return (
             <tr key={ activity.Id }>
-                <td><input type="text" value={ activity.Id } /></td>
-                <td><input type="text" value={ activity.Name } /></td>
-                <td><input type="text" value={ activity.Date } /></td>
-                <td><input type="text" value={ activity.Duration } /></td>
-                <td><input type="text" value={ activity.ActivityType } /></td>
-                <td><input type="text" value={ activity.WatchedInCinema === true ? 'yes' : 'no' } /></td>
+                <td>{ activity.Id }</td>
+                <td><input type="text" data-activityid={ activity.Id } name="Name" defaultValue={ activity.Name } onBlur={ self.onBlurChangeEvent } /></td>
+                <td><input type="text" name="Date" value={ activity.Date } /></td>
+                <td><input type="text" name="Date" value={ activity.Duration } /></td>
+                <td><input type="text" name="Date" value={ activity.ActivityType } /></td>
+                <td><input type="checkbox" name="Date" checked={ activity.WatchedInCinema } /></td>
             </tr>
             );
         });
