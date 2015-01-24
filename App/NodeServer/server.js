cb2663436d5214656b7bfd4ddc067c0c8af74479
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-01-24 12:12:23 +0100

    Node server returning all Activities.

--- App/NodeServer/server.js
+++ App/NodeServer/server.js
@@ -1,7 +1,31 @@
 var http = require("http");
 
+var activityData = [
+    {
+        Id: 1,
+        Name: 'Jurassic Park',
+        Date: '2014-01-01',
+        Duration: 120,
+        ActivityType: 'Movie',
+        WatchedInCinema: false
+    },
+    {
+        Id: 1,
+        Name: 'Jurassic Park II',
+        Date: '2014-01-02',
+        Duration: 130,
+        ActivityType: 'Movie',
+        WatchedInCinema: true
+    }];
+
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+    if (/^\/activities/.test(request.url)){
+        response.writeHead(200, {"Content-Type": "application/json"});
+        //response.write("Hello World");
+        response.end(JSON.stringify(activityData));
+    }
+    else {
+        response.writeHead(404);
+        response.end();
+    }
 }).listen(8888);
