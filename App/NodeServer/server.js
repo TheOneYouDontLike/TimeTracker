04881cb5cd87164f2bb2fbc8cd0cee703dcd8105
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-01-25 21:36:55 +0100

    Node server poor man's static files serving.

--- App/NodeServer/server.js
+++ App/NodeServer/server.js
@@ -1,4 +1,7 @@
-var http = require("http");
+'use strict';
+
+var http = require('http');
+var fs = require('fs');
 
 var activityData = [
     {
@@ -18,18 +21,45 @@ var activityData = [
         WatchedInCinema: true
     }];
 
+var indexPage = fs.readFileSync('../Web/index.html');
+var bundleJs = fs.readFileSync('../Web/bundle.js');
+var bootstrap = fs.readFileSync('../Web/node_modules/bootstrap/dist/css/bootstrap.css');
+var bootstrapMap = fs.readFileSync('../Web/node_modules/bootstrap/dist/css/bootstrap.css.map');
+
 http.createServer(function(request, response) {
     switch(true){
+        case request.url === '/':
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(indexPage);
+            break;
+
+        case request.url === '/bundle.js':
+            response.writeHead(200, {'Content-Type': 'application/javascript'});
+            response.end(bundleJs);
+            break;
+
+        case request.url === '/vendor/bootstrap.css':
+            response.writeHead(200, {'Content-Type': 'text/css'});
+            response.end(bootstrap);
+            break;
+
+        case request.url === '/vendor/bootstrap.css.map':
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end(bootstrapMap);
+            break;
+
         case /^\/activities\/[0-9]+/.test(request.url):
             logRequestToConsole(request);
-            response.writeHead(200, {"Content-Type": "application/json"});        
+            response.writeHead(200, {"Content-Type": "application/json"});
             response.end(JSON.stringify(activityData[0]));
             break;
+        
         case /^\/activities/.test(request.url):
             logRequestToConsole(request);
             response.writeHead(200, {"Content-Type": "application/json"});        
             response.end(JSON.stringify(activityData));
             break;
+        
         default:
             console.log('not found route: ');
             logRequestToConsole(request);
@@ -38,7 +68,9 @@ http.createServer(function(request, response) {
     }
 }).listen(8888);
 
+console.log('Starting localhost:8888');
+
 function logRequestToConsole(request){
     console.log(request.method);
-    console.log(request.url);
+    console.log(request.url + '\n');
 }
