6f85147c015e68d7619415269565b1be771068f1
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-01-27 21:59:21 +0100

    Refactor of router.js

--- App/NodeServer/router.js
+++ App/NodeServer/router.js
@@ -3,9 +3,9 @@ var _ = require('lodash');
 
 var router = function() {
     var unicorn = {};
-    var routingBoard = [];
+    var _routingBoard = [];
 
-    var createDestination = function(method, route, callback) {
+    var _createDestination = function(method, route, callback) {
         return {
             method: method,
             path: route,
@@ -13,51 +13,57 @@ var router = function() {
         };
     };
 
-    var removeDuplicatesInRoutingBoard = function(method, route) {
-        var thereAreSomeDuplicates = _.some(routingBoard, { method: method, path: route});
+    var _removeDuplicatesInRoutingBoard = function(method, route) {
+        var thereAreSomeDuplicates = _.some(_routingBoard, { method: method, path: route});
 
         if(thereAreSomeDuplicates) {
-            _.remove(routingBoard, function(element) {
+            _.remove(_routingBoard, function(element) {
                 return element.method === method && element.path === route;
             });
         }
     };
 
-    unicorn.httpGet = function(route, callback) {
-        removeDuplicatesInRoutingBoard('GET', route);
+    var _findRoute = function(request) {
+        var foundElement = _.find(_routingBoard, function(element){
+            return element.method === request.method && element.path === request.url;
+        });
 
-        var destination = createDestination('GET', route, callback);
-        routingBoard.push(destination);
+        return foundElement;
+    };
+
+    unicorn.httpGet = function(route, callback) {
+        _removeDuplicatesInRoutingBoard('GET', route);
+
+        var destination = _createDestination('GET', route, callback);
+        _routingBoard.push(destination);
     };
 
     unicorn.httpPost = function(route, callback) {
-        removeDuplicatesInRoutingBoard('POST', route);
+        _removeDuplicatesInRoutingBoard('POST', route);
 
-        var destination = createDestination('POST', route, callback);
+        var destination = _createDestination('POST', route, callback);
 
-        routingBoard.push(destination);
+        _routingBoard.push(destination);
     };
 
     unicorn.httpPut = function(route, callback) {
-        removeDuplicatesInRoutingBoard('PUT', route);
+        _removeDuplicatesInRoutingBoard('PUT', route);
 
-        var destination = createDestination('PUT', route, callback);
+        var destination = _createDestination('PUT', route, callback);
 
-        routingBoard.push(destination);
+        _routingBoard.push(destination);
     };
 
     unicorn.httpDelete = function(route, callback) {
-        removeDuplicatesInRoutingBoard('DELETE', route);
+        _removeDuplicatesInRoutingBoard('DELETE', route);
 
-        var destination = createDestination('DELETE', route, callback);
+        var destination = _createDestination('DELETE', route, callback);
 
-        routingBoard.push(destination);
+        _routingBoard.push(destination);
     };
 
     unicorn.route = function(request, response) {        
-        var route = _.find(routingBoard, function(element){
-            return element.method === request.method && element.path === request.url;
-        });
+        var route = _findRoute(request);
 
         if(_.isUndefined(route)){
             console.log('path does not exist: ' + request.url);
@@ -70,7 +76,7 @@ var router = function() {
         route.callback(request, response);
     };
 
-    unicorn.routingBoard = routingBoard;
+    unicorn.routingBoard = _routingBoard;
 
     return unicorn;
 };
