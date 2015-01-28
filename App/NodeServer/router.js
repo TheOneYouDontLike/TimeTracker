57630db276911ad0b68ee690c28d661a4a7be1fc
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-01-28 20:50:57 +0100

    Node router refactor - segregating methods.

--- App/NodeServer/router.js
+++ App/NodeServer/router.js
@@ -12,63 +12,6 @@ var router = function() {
     unicorn.httpDelete = httpDelete;
     unicorn.route = route;
 
-    var _createDestination = function(method, route, callback) {
-        return {
-            method: method,
-            path: route,
-            callback: callback
-        };
-    };
-
-    var _removeDuplicatesInRoutingBoard = function(method, route) {
-        var thereAreSomeDuplicates = _.some(_routingBoard, { method: method, path: route });
-
-        if(thereAreSomeDuplicates) {
-            _.remove(_routingBoard, function(element) {
-                return element.method === method && element.path === route;
-            });
-        }
-    };
-
-    var _findRoute = function(request) {
-        var foundElement = _.find(_routingBoard, function(element) {
-            return element.method === request.method && element.path === request.url;
-        });
-
-        if(_.isUndefined(foundElement)) {
-            if(!_.endsWith(request.url, '/')) {
-                var indexOfLastSlash = _.lastIndexOf(request.url, '/');
-                var lastSliceOfUrlLength = _.slice(request.url, indexOfLastSlash).length;
-
-                var url = _.dropRight(request.url, lastSliceOfUrlLength);
-                var urlString = '';
-                _.forEach(url, function(element) {
-                    urlString += element;
-                });
-
-                var urlToSearch = urlString + '/' + '{id}';
-                console.log(urlToSearch);
-                foundElement = _.find(_routingBoard, function(element) {
-                    return element.method === request.method && element.path === urlToSearch;
-                });
-
-            }
-            // var indexOfLastSlash = _.lastIndexOf(request.url, '/');
-            // var lastSliceOfUrl = _.slice(request.url, indexOfLastSlash);
-            // var urlWithoutLastSlice = _.
-
-            // if(!_.isEmpty(lastSliceOfUrl)) {
-
-            //     foundElement = _.find(_routingBoard, function(element) {
-            //         return element.method === request.method && element.path === request.url;
-            //     });
-
-            // }
-        }
-
-        return foundElement;
-    };
-
     function httpGet(route, callback) {
         _removeDuplicatesInRoutingBoard('GET', route);
 
@@ -100,10 +43,28 @@ var router = function() {
         _routingBoard.push(destination);
     }
 
-    function route(request, response) {        
-        var route = _findRoute(request);
+    function _removeDuplicatesInRoutingBoard(method, route) {
+        var thereAreSomeDuplicates = _.some(_routingBoard, { method: method, path: route });
 
-        if(_.isUndefined(route)){
+        if(thereAreSomeDuplicates) {
+            _.remove(_routingBoard, function(element) {
+                return element.method === method && element.path === route;
+            });
+        }
+    }
+
+    function _createDestination(method, route, callback) {
+        return {
+            method: method,
+            path: route,
+            callback: callback
+        };
+    }
+
+    function route(request, response) {        
+        var routeFromRoutingBoard = _findRoute(request);
+
+        if(_.isUndefined(routeFromRoutingBoard)){
             console.log('path does not exist: ' + request.url);
             response.writeHead(404);
             response.end();
@@ -111,7 +72,43 @@ var router = function() {
         }
 
         console.log('routing with route: ' + request.method + ' ' + request.url);
-        route.callback(request, response);
+        routeFromRoutingBoard.callback(request, response);
+    }
+
+    function _findRoute(request) {
+        var foundElement = _findRegularRoute(request.method, request.url);
+
+        if(_.isUndefined(foundElement)) {
+            foundElement = _findWildcardRoute(request.method, request.url);
+        }
+
+        return foundElement;
+    }
+
+    function _findRegularRoute(requestMethod, requestUrl) {
+        return _.find(_routingBoard, function(element) {
+            return element.method === requestMethod && element.path === requestUrl;
+        });
+    }
+
+    function _findWildcardRoute(requestMethod, requestUrl) {
+        if(!_.endsWith(requestUrl, '/')) {
+            var indexOfLastSlash = _.lastIndexOf(requestUrl, '/');
+            var lastSliceOfUrlLength = _.slice(requestUrl, indexOfLastSlash).length;
+
+            var url = _.dropRight(requestUrl, lastSliceOfUrlLength);
+            var urlString = '';
+            _.forEach(url, function(element) {
+                urlString += element;
+            });
+
+            var urlToSearch = urlString + '/' + '{id}';
+            
+            return _.find(_routingBoard, function(element) {
+                return element.method === requestMethod && element.path === urlToSearch;
+            });
+
+        }
     }
 
     return unicorn;
