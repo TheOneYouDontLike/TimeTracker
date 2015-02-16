13ead34012fbfeae80ba33f95cb0b52164a6e829
TheOneYouDontLike <theoneyoudontlike@gmail.com>
2015-02-16 23:40:01 +0100

    Node server - mocking and testing something with rewire.

--- App/NodeServer/tests/data.spec.js
+++ App/NodeServer/tests/data.spec.js
@@ -3,9 +3,46 @@
 var sinon = require('sinon');
 var assert = require('node-assertthat');
 var rewire = require('rewire');
+var ActivitiesData = rewire('../activities-data.js');
+
+var fsMock = {
+    exists: function(databaseName, callback) {
+        if (databaseName === 'existingDatabaseName') {
+            callback(true);
+        } else {
+            callback(false);
+        }
+
+    },
+    writeFile: function(databaseName, data, callback) {
+        callback(null);
+    }
+};
+
+ActivitiesData.__set__('fs', fsMock);
 
 describe('test', function() {
-    it('should work', function() {
-        assert.that(rewire, is.not.null());
+    it('should not init the database if does exist', function() {
+        // given
+        var activitiesData = new ActivitiesData('existingDatabaseName');
+        var callbackSpy = sinon.spy();
+
+        // when
+        activitiesData.init(callbackSpy);
+
+        // then
+        assert.that(callbackSpy.calledOnce, is.false());
+    });
+
+    it.skip('should init the database if does not exist', function() {
+        // given
+        var activitiesData = new ActivitiesData('nonExistingDatabaseName');
+        var callbackSpy = sinon.spy();
+
+        // when
+        activitiesData.init(callbackSpy);
+
+        // then
+        assert.that(callbackSpy.calledOnce, is.true());
     });
 });
